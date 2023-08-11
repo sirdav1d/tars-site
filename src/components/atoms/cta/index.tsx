52100ff71e2f595aac39b1ac5e0454bfe81dfae6
sirdav1d <ddavid.diniz@gmail.com>
2023-08-11 01:29:06 -0300

    fix: :lipstick: tamanho da fonte da CTA

--- src/components/atoms/cta/index.tsx
+++ src/components/atoms/cta/index.tsx
@@ -10,9 +10,9 @@ const montSerrat = Montserrat({
 export default function CTA() {
 	return (
 		<div className='mt-20 flex  flex-col w-full items-center justify-center gap-6 text-center rounded-sm bg-brand-red-500 p-6'>
-			<h2 className={`text-2xl md:text-4xl font-bold ${montSerrat.className} `}>
+			<h2 className={`text-2xl md:text-4xl font-bold ${montSerrat.className} max-w-2xl`}>
 				Venha descobrir como um site
-				<br /> pode te ajudar a vender mais
+			 pode te ajudar a vender mais
 			</h2>
 			<Button
 				role='CTA'
