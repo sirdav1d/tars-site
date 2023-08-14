8a71c41d3a5731fbf7031f8558fae932235f083b
sirdav1d <ddavid.diniz@gmail.com>
2023-08-14 02:01:47 -0300

    fix: :lipstick: responsividade na seção hero ajustada

--- src/components/organisms/HeroSection/bghero/index.tsx
+++ src/components/organisms/HeroSection/bghero/index.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function BgHero() {
 	return (
-		<div className='w-screen object-scale-down bg-gradient-to-b from-brand-neutral-900/10 to to-brand-neutral-900/20 absolute left-0 top-0 -z-10 h-screen md:bg-gradient-to-b md:from-brand-neutral-900/10 to md:to-brand-neutral-900/80 pb-[100%] lg:pb-[56,25%]'>
+		<div className='w-screen bg-gradient-to-b from-brand-neutral-900/10 to to-brand-neutral-900/50 absolute left-0 top-0 -z-10 h-screen md:bg-gradient-to-b md:from-brand-neutral-900/10 to md:to-brand-neutral-900/80 pb-[100%] lg:pb-[56,25%]'>
 			<video
-				className='h-screen  w-screen object-cover object-bottom lg:object-center mix-blend-overlay'
+				className='h-screen  w-screen object-fill lg:object-cover object-bottom lg:object-center mix-blend-overlay'
 				autoPlay={true}
 				preload='auto'
 				muted
