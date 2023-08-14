91ab07c4f1245422dacd47754256f47611a2fea6
sirdav1d <ddavid.diniz@gmail.com>
2023-08-14 01:56:24 -0300

    fix: :lipstick: teste de funcioanmento responsivo da imagem de poster

--- src/components/organisms/HeroSection/bghero/index.tsx
+++ src/components/organisms/HeroSection/bghero/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function BgHero() {
 	return (
-		<div className='w-screen bg-gradient-to-b from-brand-neutral-900/10 to to-brand-neutral-900/30 absolute left-0 top-0 -z-10 h-screen md:bg-gradient-to-b md:from-brand-neutral-900/10 to md:to-brand-neutral-900/80'>
+		<div className='w-screen object-scale-down bg-gradient-to-b from-brand-neutral-900/10 to to-brand-neutral-900/20 absolute left-0 top-0 -z-10 h-screen md:bg-gradient-to-b md:from-brand-neutral-900/10 to md:to-brand-neutral-900/80 pb-[100%] lg:pb-[56,25%]'>
 			<video
 				className='h-screen  w-screen object-cover object-bottom lg:object-center mix-blend-overlay'
 				autoPlay={true}
@@ -11,7 +11,7 @@ export default function BgHero() {
 				poster='/images/poster.png'
 				loop={true}>
 				<source
-					src='/video/vídeoponte.mp4'
+					// src='/video/vídeoponte.mp4'
 					type='video/mp4'
 				/>
 			</video>
