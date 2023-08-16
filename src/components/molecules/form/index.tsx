ed0d6fb051d3978309ec6ca215bb080cedd6f56c
sirdav1d <ddavid.diniz@gmail.com>
2023-08-16 06:52:09 -0300

    style: :lipstick: ajuste fino nos estilos do formulário

--- src/components/molecules/form/index.tsx
+++ src/components/molecules/form/index.tsx
@@ -15,13 +15,13 @@ export default function Form() {
 	const [tel, setTel] = useState('');
 
 	return (
-		<form className='bg-brand-neutral-900/50 backdrop-blur-md p-8 border border-brand-neutral-100/10 rounded-md shadow-xl max-w-[368px] lg:max-w-full'>
+		<form className='bg-brand-neutral-800/50 backdrop-blur-sm p-8 border border-brand-neutral-100/10 rounded-md shadow-xl max-w-[368px] lg:max-w-full'>
 			<div className='flex flex-col items-center justify-center pb-5'>
 				<h2
 					className={`${montSerrat.className} text-center text-2xl`}>
 					Entre em contato e faça seu Orçamento
 				</h2>
-				<span className='w-[80px] h-2 bg-brand-blue-500 rounded-full mt-1 drop-shadow-icons' />
+				<span className='w-[80px] lg:w-[120px] h-2 bg-brand-blue-500 rounded-full mt-1 drop-shadow-icons' />
 			</div>
 			<FormItem
 				value={name}
