b1fa842de33a21e831bdf9749684361209073870
sirdav1d <ddavid.diniz@gmail.com>
2023-08-20 04:45:38 -0300

    fix: :lipstick: padding desnecessário excluido

--- src/components/organisms/HeroSection/bghero/index.tsx
+++ src/components/organisms/HeroSection/bghero/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function BgHero() {
 	return (
-		<div className='w-screen bg-gradient-to-b from-brand-neutral-900/10 to to-brand-neutral-900/50 absolute left-0 top-0 -z-10 h-screen md:bg-gradient-to-b md:from-brand-neutral-900/10 to md:to-brand-neutral-900/80 pb-[100%] lg:pb-[56,25%]'>
+		<div className='w-screen bg-gradient-to-b from-brand-neutral-900/10 to to-brand-neutral-900/50 absolute left-0 top-0 -z-10 h-screen md:bg-gradient-to-b md:from-brand-neutral-900/10 to md:to-brand-neutral-900/80'>
 			<video
 				className='h-screen  w-screen object-fill lg:object-cover object-center mix-blend-overlay'
 				autoPlay={true}
