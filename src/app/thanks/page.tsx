5f8eda3bffba22ea95a729c7382801cf5f06e367
sirdav1d <ddavid.diniz@gmail.com>
2023-09-02 02:47:51 -0300

    fix: :lipstick: ajuste de espaçamentos

--- src/app/thanks/page.tsx
+++ src/app/thanks/page.tsx
@@ -23,8 +23,8 @@ export default function ThanksPage() {
 					width={480}
 					height={100}
 					alt='logo tars projetos'></Image>
-				<div className='flex my-12 mx-0 lg:m-16 gap-10 flex-col lg:flex-row'>
-					<div className='flex w-full  flex-col gap-5'>
+				<div className='flex my-12 mx-0 lg:m-16 gap-10 flex-col lg:flex-row max-w-[1024px] items-center justify-center'>
+					<div className='flex w-full lg:w-2/3  flex-col gap-5'>
 						<div className='flex flex-col gap-4'>
 							<span className='w-[120px] h-2 bg-brand-blue-500 rounded-full mt-1 drop-shadow-icons' />
 							<h2 className={`${montSerrat.className} text-5xl font-extrabold`}>
@@ -34,23 +34,23 @@ export default function ThanksPage() {
 						<h3 className='text-lg font-semibold'>
 							Nosso time comercial entrará em contato em alguns instantes
 						</h3>
-						<p className='text-base font-normal mt-5'>
-							Para agradecer pela sua confiança, disponibilizamos um ebook
+						<p className='text-base font-normal mt-5 leading-relaxed'>
+							Para agradecer pela sua confiança, disponibilizamos um e-book
 							gratuito que pode te ajudar a entender a importância do SEO
 							(Ferramenta de Otimização de Pesquisa) do Google, no seu site, ou
 							da sua empresa.{' '}
 						</p>
 					</div>
-					<div className='flex flex-col w-full items-center lg:items-start justify-center'>
+					<div className='flex flex-col w-full lg:w-1/3 items-center justify-center'>
 						<Image
-							className='motion-reduce:animate-bounce'
+							className='animate-pulse'
 							src={'/images/trafegoImage.png'}
 							width={280}
 							height={320}
 							alt='Capa do livro, o manual do tráfego'
 						/>
 						<Button
-							text={'Baixar conteúdo gratuito'}
+							text={'Baixar Grátis'}
 							role='thanks'
 						/>
 					</div>
