9b05043b54644911fa71b8f1e505d13459fb2171
sirdav1d <ddavid.diniz@gmail.com>
2023-09-22 02:41:38 -0300

    fix: :speech_balloon: Ajuste de copy

--- src/components/atoms/loading/index.tsx
+++ src/components/atoms/loading/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function LoadingBtn() {
 	return (
 		<div>
-			<div className='w-6 h-6 rounded-full animate-spin'>
+			<div className='w-5 h-5 rounded-full animate-spin'>
 				<svg
 					fill='#ffffff'
 					viewBox='0 0 16 16'
--- src/components/molecules/form/index.tsx
+++ src/components/molecules/form/index.tsx
@@ -84,8 +84,7 @@ export default function FormComponent() {
 					<Form className='bg-brand-neutral-800/50 backdrop-blur-sm p-8 border border-brand-neutral-100/10 rounded-md shadow-xl max-w-[368px] lg:max-w-full'>
 						<div className='flex flex-col items-center justify-center pb-5'>
 							<h2 className={`${montSerrat.className} text-center text-xl`}>
-								Descubra Como Transformamos Visitantes em
-								Clientes!
+								Descubra o Potencial do Seu Negócio!
 							</h2>
 							<span className='w-[80px] lg:w-[120px] h-2 bg-brand-blue-500 rounded-full mt-1 drop-shadow-icons' />
 						</div>
