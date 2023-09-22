37b87cefff74fc5f66bd071b0ca3cf892ee89b78
sirdav1d <ddavid.diniz@gmail.com>
2023-09-22 02:54:05 -0300

    chore: :arrow_up: atualização de dependencias

--- src/components/organisms/PlanSection/index.tsx
+++ src/components/organisms/PlanSection/index.tsx
@@ -32,19 +32,19 @@ export default function PlanSection() {
 
 			<div className='flex flex-col xl:flex-row px-40 gap-12 items-center justify-center pb-10'>
 				<PlanCard
-					key={String(benefitsInitial.text)}
+					key={String(benefitsInitial.text[0])}
 					role='Iniciante'
 					style={benefitsInitial.style}
 					text={benefitsInitial.text}
 				/>
 				<PlanCard
-					key={String(benefitsPro.text)}
+					key={String(benefitsPro.text[0])}
 					role='Profissional'
 					style={benefitsPro.style}
 					text={benefitsPro.text}
 				/>
 				<PlanCard
-					key={String(benefitsBussiness.text)}
+					key={String(benefitsBussiness.text[0])}
 					role='Empresarial'
 					style={benefitsBussiness.style}
 					text={benefitsBussiness.text}
