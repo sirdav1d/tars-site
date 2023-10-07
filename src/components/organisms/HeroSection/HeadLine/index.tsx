12b5f740571f2d588a2af4fbdcb2481d1cd586db
sirdav1d <ddavid.diniz@gmail.com>
2023-10-07 04:04:59 -0300

    feat: :zap: ajuste de transição da animação

--- src/components/organisms/HeroSection/HeadLine/index.tsx
+++ src/components/organisms/HeroSection/HeadLine/index.tsx
@@ -6,14 +6,15 @@ export default function Headline() {
 		<div className='flex flex-col gap-4'>
 			<motion.h1
 				animate={{ opacity: 1, x: 0 }}
-				initial={{ opacity: 0, x: -200 }}
+				initial={{ opacity: 0, x: -150 }}
+				transition={{ delay: 0.2, duration: 0.4 }}
 				className={`font-bold max-w-[780px] text-4xl xl:text-5xl mx-auto text-brand-neutral-50 2xl:text-6x text-left md:text-center `}>
 				Construímos pontes entre sua marca e seu público
 			</motion.h1>
 			<motion.p
 				animate={{ opacity: 1, x: 0 }}
-				initial={{ opacity: 0, x: -100 }}
-				transition={{ delay: 0.3 }}
+				initial={{ opacity: 0, x: -150 }}
+				transition={{ delay: 0.6, duration: 0.4 }}
 				className='text-sm md:text-base lg:text-xl text-left md:text-center font-semibold max-w-[300px] md:max-w-full  md:mx-auto  text-brand-neutral-100'>
 				Criamos experiências digitais que inspiram e conectam
 			</motion.p>
