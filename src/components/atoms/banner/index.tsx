e39ce778f37be2ff770a3cb8eba6308a18896e5d
sirdav1d <ddavid.diniz@gmail.com>
2023-11-25 01:12:17 -0300

    feat: :zap: ajuste de banner

--- src/components/atoms/banner/index.tsx
+++ src/components/atoms/banner/index.tsx
@@ -2,6 +2,7 @@
 
 'use client';
 import { getLocalStorage, setLocalStorage } from '@/helpers/storageHelper';
+import { AnimatePresence, motion } from 'framer-motion';
 import { useEffect, useState } from 'react';
 
 export default function BannerLGPD() {
@@ -35,46 +36,52 @@ export default function BannerLGPD() {
 	}
 
 	return (
-		<div
-			className={`fixed bottom-0 center ${
-				!cookieConsent ? 'flex' : 'hidden'
-			}  items-center w-full mx-auto h-fit text-brand-neutral-100 text-xs lg:text-sm p-3  z-[999999999999999]`}>
-			<div className=' max-w-[1024px]  rounded-lg gap-1 lg:gap-5 shadow-lg flex flex-col lg:flex-row justify-center items-center mx-auto p-5 bg-brand-neutral-950 lg:py-8 lg:px-8'>
-				<p>
-					Nós utilizamos cookies para melhorar sua experiência como usuário.
-					Para conferir detalhadamente nossas boas práticas, acesse nossas{' '}
-					<a
-						className='hover:text-brand-red-500 transition duration-300 underline'
-						target='_blank'
-						href='/resources/Politica.pdf'>
-						Políticas de Privacidade
-					</a>{' '}
-					e{' '}
-					<a
-						className='hover:text-brand-red-500 transition duration-300 underline'
-						target='_blank'
-						href='/resources/Termos.pdf'>
-						Termos de uso
-					</a>
-					.
-				</p>
-				<div className='gap-3 lg:gap-5 flex justify-center items-center mx-auto'>
-					<button
-						onClick={() => handleAccept()}
-						type='button'
-						className={`w-fit px-4 py-2 lg:w-28 lg:h-12 bg-brand-red-700 rounded-full z-50 disabled:grayscale disabled:hover:bg-brand-red-500 flex gap-2 items-center justify-center text-center hover:bg-brand-red-500  transition duration-300 mt-3 lg:text-base text-xs font-bold tracking-wide`}>
-						<p className='flex gap-3 items-center justify-center drop-shadow'>Aceitar</p>
-					</button>
-					<button
-						onClick={() => handleDeny()}
-						type='button'
-						className={`flex items-center justify-center w-fit text-center hover:opacity-80 transition duration-300 mt-3 lg:text-base text-xs font-bold tracking-wide`}>
-						<p className='flex gap-3 items-center justify-center underline'>
-							Recusar
-						</p>
-					</button>
+		<AnimatePresence
+			mode='wait'
+			presenceAffectsLayout={true}>
+			<motion.div
+				className={`fixed bottom-0 center ${
+					!cookieConsent ? 'flex' : 'hidden'
+				}  items-center w-full mx-auto h-fit text-brand-neutral-100 text-xs lg:text-sm p-3  z-[999999999999999]`}>
+				<div className=' max-w-[1024px]  rounded-lg gap-1 lg:gap-5 shadow-lg flex flex-col lg:flex-row justify-center items-center mx-auto p-5 bg-brand-neutral-950 lg:py-8 lg:px-8'>
+					<p>
+						Nós utilizamos cookies para melhorar sua experiência como usuário.
+						Para conferir detalhadamente nossas boas práticas, acesse nossas{' '}
+						<a
+							className='hover:text-brand-red-500 transition duration-300 underline'
+							target='_blank'
+							href='/resources/Politica.pdf'>
+							Políticas de Privacidade
+						</a>{' '}
+						e{' '}
+						<a
+							className='hover:text-brand-red-500 transition duration-300 underline'
+							target='_blank'
+							href='/resources/Termos.pdf'>
+							Termos de uso
+						</a>
+						.
+					</p>
+					<div className='gap-3 lg:gap-5 flex justify-center items-center mx-auto'>
+						<button
+							onClick={() => handleAccept()}
+							type='button'
+							className={`w-fit px-4 py-2 lg:w-28 lg:h-12 bg-brand-red-700 rounded-full z-50 disabled:grayscale disabled:hover:bg-brand-red-500 flex gap-2 items-center justify-center text-center hover:bg-brand-red-500  transition duration-300 mt-3 lg:text-base text-xs font-bold tracking-wide`}>
+							<p className='flex gap-3 items-center justify-center drop-shadow'>
+								Aceitar
+							</p>
+						</button>
+						<button
+							onClick={() => handleDeny()}
+							type='button'
+							className={`flex items-center justify-center w-fit text-center hover:opacity-80 transition duration-300 mt-3 lg:text-base text-xs font-bold tracking-wide`}>
+							<p className='flex gap-3 items-center justify-center underline'>
+								Recusar
+							</p>
+						</button>
+					</div>
 				</div>
-			</div>
-		</div>
+			</motion.div>
+		</AnimatePresence>
 	);
 }
