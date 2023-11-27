3e6c187dd4885c45fdc44ec83a59652fa577c7af
sirdav1d <ddavid.diniz@gmail.com>
2023-11-27 09:55:26 -0300

    fix: :lipstick: ajuste de grid na heroSection

--- src/components/organisms/HeroSection/index.tsx
+++ src/components/organisms/HeroSection/index.tsx
@@ -6,9 +6,9 @@ export default function HeroSection() {
 	return (
 		<main
 			id='home'
-			className=' h-screen bg-cover bg-bgHero flex items-center md:px-14 px-5'>
-			<div className='flex flex-col w-full gap-40 md:gap-8 lg:gap-0  max-w-[1440px] md:mx-auto justify-between items-center h-full'>
-				<div className='flex mt-14 md:mt-0 items-center justify-between drop-shadow-md relative h-full'>
+			className=' h-screen bg-cover bg-bgHero flex items-center md:px-14 px-4 '>
+			<div className='flex flex-col w-full gap-40 md:gap-8 lg:gap-0   max-w-[1440px] md:mx-auto justify-between items-center h-full'>
+				<div className='flex mt-14 items-center justify-between drop-shadow-md relative h-full '>
 					<div className='flex flex-col gap-28 md:gap-40 '>
 						<Headline />
 
